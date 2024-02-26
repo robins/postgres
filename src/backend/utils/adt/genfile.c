bfd88b93b58db3bd842f4ecb15c444c415c45542
Robins Tharakan <tharakan@gmail.com>
2024-02-26 18:42:40 +1030

    Catversion bump. Fix 2 other typos in previous commit

--- src/backend/utils/adt/genfile.c
+++ src/backend/utils/adt/genfile.c
@@ -745,6 +745,15 @@ pg_ls_archive_statusdir(PG_FUNCTION_ARGS)
 }
 
 /*
+ * Function to return the list of files in the pg_logical/snapshots directory.
+ */
+Datum
+pg_ls_logicalsnapdir(PG_FUNCTION_ARGS)
+{
+	return pg_ls_dir_files(fcinfo, "pg_logical/snapshots", false);
+}
+
+/*
  * Function to return the list of files in the pg_logical/mappings directory.
  */
 Datum
